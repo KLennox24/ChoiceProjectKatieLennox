fb8035871c0b3448871063eeddbef006999022d5
Katherine Lennox <klennox24@biomedscienceacademy.org>
2020-12-11 15:53:54 +0000

    Scenario one input, comment fix

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
+
+scenario_one_print = input("You were camping one day, when a bear was found roaming around your tent. Unfortunately, it appears to be agressive, so you don't have much choice but to fight the bear, or run. Type Fight or Flight to decide your fate.")
+
+
 def scenario_one():
-  print("hi")
+  print("test")
 
-print("hi")
 
-"""
-scenario_two(fight,flight)
-if flight_one:
-Buff lumberjack laughs at the decision and challenges you to a duel
-fight_two, flight_two
-else:
-Passing lumberjack nods in respect after you fought the bear
-pass_two (because its part of scenario two)
-scenario_three(flight_two, fight_two, pass_two)
-if flight_two:
-You have been shunned from your village for cowardice. Hopefully that doesn’t become important later. Try to disagree with village or no
-flight_three fight_three
-if fight_two:
-Although you lost the fight, the mayor of your village gives you an award for trying. Usually, they run from fear, so it was a commendable effort.
-pass_three
-if pass_two:
-pass_three
-scenario_four(fight_three, flight_three, pass_three)
-if fight three:
-pass_four
-if flight three:
-pass_three
-if pass_four:
-Fight_four or flight_four
-scenario_five(fight_four, flight_four, pass_four)
-if fight_four:
-flight_five
-if flight_four:
-pass_five
-if pass_four:
-flight_five or fight_five
-"""
+#scenario_two(fight,flight)
+#if flight_one:
+#Buff lumberjack laughs at the decision and challenges you
+#to a duel
+#fight_two, flight_two
+#else:
+#Passing lumberjack nods in respect after you fought the
+#bear
+#pass_two (because its part of scenario two)
+#scenario_three(flight_two, fight_two, pass_two)
+#if flight_two:
+#You have been shunned from your village for cowardice.
+#Hopefully that doesn’t become important later. Try to
+#disagree with village or no
+#flight_three fight_three
+#if fight_two:
+#Although you lost the fight, the mayor of your village
+#gives you an award for trying. Usually, they run from
+#fear, so it was a commendable effort.
+#pass_three
+#if pass_two:
+#pass_three
+#scenario_four(fight_three, flight_three, pass_three)
+#if fight three:
+#pass_four
+#if flight three:
+#pass_three
+#if pass_four:
+#Fight_four or flight_four
+#scenario_five(fight_four, flight_four, pass_four)
+#if fight_four:
+#flight_five
+#if flight_four:
+#pass_five
+#if pass_four:
+#flight or fight five
