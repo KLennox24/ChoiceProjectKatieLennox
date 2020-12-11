3a81cd69acf0f30540aa5291fc0614a551237c6f
Katherine Lennox <klennox24@biomedscienceacademy.org>
2020-12-11 22:01:41 +0000

    Scenario two spacing fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-explanation_check = input("Welcome to Fight or Flight! Have you played the game before? Type 'Y' if yes, 'N' if no.\n")
+explanation_check = input("Welcome to Fight or Flight! Have you played the game before? Type 'Y' if\nyes, 'N' if no.\n")
 
 def game_explanation():
   print("\nIn this game, you will answer different scenarios you are presented with by answering either Fight or Flight. Some scenarios do not allow you to answer them. Based on the decisions you make, your character will move through the game, dealing with your past decisions. If you type something other than\n'Fight' or 'Flight', the game will not work. If you are not given the option\nto respond, press enter to continue the game. Good luck!")
@@ -9,7 +9,7 @@ if explanation_check == "Y":
 elif explanation_check == "N":
   game_explanation()
 
-scenario_one_print = input("\nYou were camping one day, when a bear was found roaming around\nyour tent. Unfortunately, it appears to be agressive, so you\ndon't have much choice but to fight the bear, or run. Type\n'Fight' to fight the bear, or 'Flight' to run away.\n")
+scenario_one_print = input("\nYou were camping one day, when a bear was found roaming around your tent.\nUnfortunately, it appears to be agressive, so you don't have much choice\nbut to fight the bear, or run. Type 'Fight' to fight the bear, or 'Flight'\nto run away.\n")
 
 if scenario_one_print == "Fight":
   fight_one = "True"
@@ -26,9 +26,13 @@ scenario_one()
 
 def scenario_two():
   if fight_one == "True":
-    print("As you are walking downtown after your recovering, you notice a lumberjack walk by you, and give you a respectful nod. Even though you are slightly confused by this, you can not ignore the fact it feels like you barely escaped a dangerous situation. You wonder why he gave you that nod, but continue on with your day.\n")
+    print("\nThe next scenario will begin shortly.")
+    print("\nAs you are walking downtown after your recovery, you notice a lumberjack\nwalk by you, and give you a respectful nod. Even though you are slightly\nconfused by this, you can not ignore the fact it feels like you barely\nescaped a dangerous situation. You wonder why he gave you that nod, but\ncontinue on with your day.\n")
   elif fight_one == "False":
-    print("The world must not be feeling kind to you today. While walking through your neighborhood, a threatening lumberjack approaches you. He explains that he was passing by when he saw you running from the bear. This was a foolsih decision. The lumberjack challenges you to a duel! Do you choose to fight or run?")
+    print("\nThe next scenario will begin shortly.")
+    print("\nThe world must not be feeling kind to you today. While walking through your neighborhood, a threatening lumberjack approaches you. He explains that he\nwas passing by when he saw you running from the bear. This was a foolsih\ndecision. The lumberjack challenges you to a duel! Do you choose to fight or\nrun?")
+
+scenario_two()
 
 scenario_two_print = input("Type 'Fight' to accept the duel, or 'Flight' to run away.\n")
 
